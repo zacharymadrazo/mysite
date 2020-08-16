ad775646897ff79c19e96c61a19e5ae892c1a4d3
Zachary Madrazo <zachary.madrazo278@gmail.com>
2020-08-16 07:45:08 -0400

    First HTML Setup Message: "Hello Django"

---
+++ mysite/HelloDjangoApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+# Create your views here.
+def index(request):
+
+    now = datetime.now()
+
+    html_content = "<html><head><title>Hello, Django </title></head><body>"
+    html_content += "<strong>Hello, Django!</strong> on " + now.strftime("A, %d %B, %Y at %X")
+    html_content += "</body></html>"
+
+    return HttpResponse(html_content)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,9 +7,17 @@ from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+#Added From tutorial
+from django.conf.urls import include,url
+import HelloDjangoApp.views
+####
 
+#Django processes URL Patterns in the order they appear in the array
 
 urlpatterns = [
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    #End of url views
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
